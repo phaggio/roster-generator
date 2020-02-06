c95f24503784b85e6236812e298da5f7e348d5e6
Richard Wang <richardchacha@hotmail.com>
2020-02-05 16:22:54 -0800

    revise prompt questions

--- app.js
+++ app.js
@@ -1,2 +1,42 @@
-console.clear();
 'use strict';
+console.clear();
+
+const inquirer = require('inquirer');
+const prompts = require('./prompts');
+const Manager = require('./lib/Manager');
+const Engineer = require('./lib/Engineer');
+const Intern = require('./lib/Intern');
+
+
+
+
+async function init() {
+    let newHire;
+    let employee = await inquirer.prompt(prompts[0].basicQuestions);
+    let role = employee.role;
+    // console.log(employee);
+
+    let res = await inquirer.prompt(prompts[0][`${role}`]);
+    // console.log(res);
+
+    switch (role) {
+        case 'Manager':
+            newHire = new Manager(employee.name, employee.id, employee.email, res.officeNumber);
+            break;
+        case 'Engineer':
+            newHire = new Engineer(employee.name, employee.id, employee.email, res.github);
+            break;
+        case 'Intern':
+            newHire = new Intern(employee.name, employee.id, employee.email, res.school);
+            break;
+        default:
+            break;
+    }
+    console.log(newHire);
+    newHire.printInfo();
+
+}
+
+
+
+init();
